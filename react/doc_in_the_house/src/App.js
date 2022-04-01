3f85cc2dafd8b7fb14ca48929a78814ea48acda1
Akil Prasath R <akilprasathr@gmail.com>
2022-04-01 11:03:13 +0530

    Finishes Patient Selector Page designs

--- react/doc_in_the_house/src/App.js
+++ react/doc_in_the_house/src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Home } from "./views/Home";
+import { HomeContent } from "./views/Home";
+import { CharacterSelect } from "./views/CharacterSelect";
 function App() {
 	console.log("adfadfadf");
 	return (
 		<Router>
 			<Routes>
-				<Route path="/" element={<Home />}></Route>
+				<Route path="/" element={<HomeContent />}></Route>
+				<Route
+					path="/character-select"
+					element={<CharacterSelect />}></Route>
 			</Routes>
 		</Router>
 	);
